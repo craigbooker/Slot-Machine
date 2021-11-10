0c6ba79fee3b6a0ed612300df2ed1f7604e35ab2
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-11-10 16:16:11 -0600

    Refectored modifiers to Extensions file to clean up code.

--- Slot Machine/Helpers/Extensions.swift
+++ Slot Machine/Helpers/Extensions.swift
@@ -5,4 +5,21 @@
 //  Created by Craig Booker on 11/10/21.
 //
 
-import Foundation
+import SwiftUI
+
+extension Text {
+    func scoreLabelStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(size: 10, weight: .bold, design: .rounded))
+    }
+    
+    func scoreNumberStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(.title, design: .rounded))
+            .fontWeight(.heavy)
+    }
+}
+
+
