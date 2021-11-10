dc5ed11ec2074ed35513b78dca79613f5977e8e9
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-11-10 16:41:31 -0600

    Built out the basic header with score board.

--- Slot Machine/Views/ContentView.swift
+++ Slot Machine/Views/ContentView.swift
@@ -7,12 +7,91 @@
 
 import SwiftUI
 
+// MARK: - PROPERTIES
+
 struct ContentView: View {
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            // MARK: - BACKGROUND
+            LinearGradient(gradient: Gradient(colors: [Color("ColorPink"), Color("ColorPurple")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            // MARK: - INTERFACE
+            VStack(alignment: .center, spacing: 5) {
+                
+                // MARK: - HEADER
+                LogoView()
+                
+                Spacer()
+                
+                // MARK: - SCORE
+                HStack {
+                    HStack {
+                        Text("Your\nCoins".uppercased())
+                            .scoreLabelStyle()
+                            .multilineTextAlignment(.trailing)
+                        
+                        Text("100")
+                            .scoreNumberStyle()
+                            .modifier(ScoreNumberModifier())
+                    }
+                    .modifier(ScoreContainerModifier())
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Text("200")
+                            .scoreNumberStyle()
+                            .modifier(ScoreNumberModifier())
+                        
+                        Text("High\nScore".uppercased())
+                            .scoreLabelStyle()
+                            .multilineTextAlignment(.trailing)
+                        
+                        
+                    }
+                    .modifier(ScoreContainerModifier())
+                }
+                
+                // MARK: - SLOT MACHINE
+                // MARK: - FOOTER
+                
+                Spacer()
+            }
+            .overlay(
+                // RESET
+                Button(action: {
+                print("Reset the game")
+            }) {
+                Image(systemName: "arrow.2.circlepath.circle")
+            }
+            .modifier(ButtonModifier()),
+            alignment: .topLeading
+            )
+            .overlay(
+                // INFO
+                Button(action: {
+                print("Info view")
+            }) {
+                Image(systemName: "info.circle")
+            }
+            .modifier(ButtonModifier()),
+            alignment: .topTrailing
+            )
             .padding()
+            .frame(maxWidth: 720)
+            
+            // MARK: - POPUP
+            
+            
+        } //: ZSTACK
+        
     }
 }
+
+// MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
