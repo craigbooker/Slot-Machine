3890dd0fa375856e088724b5fc3d6beb4328e7a2
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-11-11 10:51:58 -0600

    Created a credits view.

--- Slot Machine/Views/InfoView.swift
+++ Slot Machine/Views/InfoView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct InfoView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 10) {
+            LogoView()
+            
+            Spacer()
+            
+            Form {
+                Section(header: Text("About the application")) {
+                    FormRowView(firstItem: "Application", secondItem: "Slot Machine")
+                    FormRowView(firstItem: "Platforms", secondItem: "iPhone, iPad, Mac")
+                    FormRowView(firstItem: "Developer", secondItem: "Craig Booker")
+                    FormRowView(firstItem: "Designer", secondItem: "Craig Booker")
+                    FormRowView(firstItem: "Music", secondItem: "Dan Lebowitz")
+                    FormRowView(firstItem: "website", secondItem: "arcadiasoftworks.com")
+                    FormRowView(firstItem: "Copyright", secondItem: "© 2021 All rights reserved.")
+                    FormRowView(firstItem: "Version", secondItem: "1.0.0")
+                }
+            }
+            .font(.system(.body, design: .rounded))
+        }
+        .padding(.top, 40)
+        .overlay(
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "xmark.circle")
+                    .font(.title)
+            }
+                .padding(.top, 30)
+                .padding(.trailing, 20)
+            , alignment: .topTrailing
+        )
+    }
+}
+
+struct FormRowView: View {
+    var firstItem: String
+    var secondItem: String
+    
+    var body: some View {
+        HStack {
+            Text(firstItem).foregroundColor(Color.gray)
+            Spacer()
+            Text(secondItem)
+        }
     }
 }
 
@@ -18,3 +63,5 @@ struct InfoView_Previews: PreviewProvider {
         InfoView()
     }
 }
+
+
